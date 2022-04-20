d326fc8cc83c2e3684e953772a7d496ad8b35c82
LanaDelSlay <gmiller1902@gmail.com>
2022-04-20 00:07:40 -0700

    Layout made, images added

---
+++ main.py
@@ -0,0 +1,28 @@
+## While bank not empty of hides. 
+
+    ## Check if inventory has hides, and both runes store the results so we know wat to grab from bank.
+
+        ## If Yes switch to magic tab.
+
+        ## If No open bank -> Check if bank is on correct tab.
+
+            ## If no click correct bank tab
+
+            ## If Yes Continue to next statement
+
+            ## Grab correct items as stored upon first starting.
+
+            ## Exit bank then go to magic tab.
+
+        ## Cast tan hide until hide is gone.
+
+        ## ^^ Once clicked 5 times (Gets rid of 25/26 of the hides if having the runes in inv w/o rune pouch)
+
+        ## Switch to inventory
+
+        ## Check if any thing is missing
+
+            ## If yes kill the script
+
+            # If no (other than the obvious hides) continue restart 
+
---
+++ utils.py
@@ -0,0 +1,36 @@
+def isDragonHideTanned():
+    print()
+    
+def recordMissingItems():
+    print()
+    
+def openMagic():
+    print()
+    
+def isMagicOpen():
+    print()
+    
+def openInventory():
+    print() 
+    
+def isInventoryOpen():
+    print()
+    
+def openBank():
+    print()
+    
+def isBankOpen():
+    print()
+    
+def openBankTab():
+    print()
+    
+def isBankTabOpen():
+    print()
+    
+def withdrawHides():
+    print()
+    
+def isHidesInBank():
+    print()
+    
