3afa8cb144e5f4c25d6ba86a0a5e0264bd1d74a4
LanaDelSlay <gmiller1902@gmail.com>
2022-04-20 21:14:44 -0700

    Bot workin'

--- main.py
+++ main.py
@@ -26,3 +26,52 @@
 
             # If no (other than the obvious hides) continue restart 
 
+from utils import *
+print('Side bar must be opened.')
+sleep(5)
+while isHidesInBank():
+    itemsNeeded = recordMissingItems()
+    print(itemsNeeded)
+    outOfAstrals = itemsNeeded.get("astrals")
+    outOfNaures = itemsNeeded.get("natures")
+    outOfHide = itemsNeeded.get("hides")
+    
+    if outOfAstrals or outOfNaures:
+        print("Ran out of runes")
+        quit()
+    
+    if outOfHide:
+        print("No hide")
+        openBank()
+        sleep(1)
+        if isBankOpen():
+            if isBankTabOpen():
+                continue
+            if not isBankTabOpen():
+                openBankTab()
+            if not isHidesInBank():
+                print("NO HIDES, KILLING SCRIPT")
+                quit()
+            withdrawHides()
+            sleep(giveDelay())
+            closeBank()
+    openMagic()
+    print("Magic opened")
+    sleep(1)
+    castTanHide()
+    print("Done tanning")
+    openBank()
+    print("Depositing")
+    sleep(.5)
+    if not isBankTabOpen():
+        print("Bank tab not open")
+        openBankTab()
+    print("Attempting to deposit")
+    depositHides()
+    sleep(.5)
+    if not isHidesInBank():
+        print("Ran out of hides")
+        quit()
+    withdrawHides()
+    sleep(1)
+    closeBank()
--- utils.py
+++ utils.py
@@ -1,36 +1,123 @@
+from time import sleep
+import pyautogui
+import pyclick
+from numpy import random
+
+bankTellerColor = (214, 183, 150)
+
+def move(x,y):
+    hc = pyclick.HumanClicker()
+    dist = random.normal(loc=2, scale=1, size=(4))
+    x = x + abs(dist[0])
+    y = y + abs(dist[1])
+    x = int(x.item())
+    y = int(y.item())
+    hc.move((x,y), abs(dist[2]))
+    sleep(abs(dist[3]))
+    hc.click()
+
+def giveDelay():
+    dist = random.normal(loc=3, scale=1, size=(4))
+    return abs(dist[0])
+
 def isDragonHideTanned():
-    print()
+    if pyautogui.locateCenterOnScreen('./imgs/tannedHide.png'):
+        return True
+    else: 
+        return False
     
 def recordMissingItems():
-    print()
-    
+    checks = {
+        "astrals": not isAstralsInInv(), 
+        "natures": not isNatureRunesInInv(),
+        "hides": isDragonHideTanned()
+             }
+    return checks
+
 def openMagic():
-    print()
+    if not isMagicOpen():
+        magicIcon = pyautogui.locateCenterOnScreen('./imgs/magicUnopened.png')#If the file is not a png file it will not work
+        move(magicIcon.x, magicIcon.y)
     
 def isMagicOpen():
-    print()
+    if pyautogui.locateCenterOnScreen('./imgs/magicOpened.png'):
+        return True
+    else:
+        return False
     
 def openInventory():
-    print() 
+    if not isInventoryOpen():
+        magicIcon = pyautogui.locateCenterOnScreen('./imgs/invUnopened.png')#If the file is not a png file it will not work
+        move(magicIcon.x, magicIcon.y) 
     
 def isInventoryOpen():
-    print()
+    if pyautogui.locateCenterOnScreen('./imgs/invOpened.png'):
+        return True
+    else:
+        return False
     
 def openBank():
-    print()
+    if not isBankOpen():
+        s = pyautogui.screenshot()
+        for x in range(0,1919,5):
+            for y in range(0,1079,5):
+                if s.getpixel((x, y)) == bankTellerColor: 
+                    move(x,y)
+                    return
     
 def isBankOpen():
-    print()
+    if pyautogui.locateCenterOnScreen('./imgs/bankOpened.png') or pyautogui.locateCenterOnScreen('./imgs/bankTabOpenedText.png'):
+        return True
+    else:
+        return False
+
+def depositHides():
+    if isBankOpen():
+        move(1477,782)
     
 def openBankTab():
-    print()
-    
+  if not isInventoryOpen() and not isBankTabOpen():
+    try:
+            bankTab = pyautogui.locateCenterOnScreen('./imgs/bankTabUnopened.png')#If the file is not a png file it will not work
+            move(bankTab.x, bankTab.y) 
+    except AttributeError:
+        print("Couldn't find tab")
 def isBankTabOpen():
-    print()
+    if pyautogui.locateCenterOnScreen('./imgs/bankTabOpened.png'):
+        return True
+    else:
+        return False
     
 def withdrawHides():
-    print()
+    if isHidesInBank():
+        hidesInBank = pyautogui.locateCenterOnScreen('./imgs/untannedHideInBank.png')#If the file is not a png file it will not work
+        move(hidesInBank.x, hidesInBank.y)
     
 def isHidesInBank():
-    print()
+    if pyautogui.locateCenterOnScreen('./imgs/untannedHidesGone.png'):
+        return False
+    else:
+        return True
     
+def isAstralsInInv():
+    if pyautogui.locateCenterOnScreen('./imgs/astralRune.png'):
+        return True
+    else:
+        return False
+    
+def isNatureRunesInInv():
+    if pyautogui.locateCenterOnScreen('./imgs/natureRune.png'):
+        return True
+    else:
+        return False
+    
+def closeBank():
+    pyautogui.press('escape')
+    
+def castTanHide():
+    hc = pyclick.HumanClicker()
+    spellLocation = pyautogui.locateCenterOnScreen('./imgs/tanHideSpell.png')#If the file is not a png file it will not work
+    move(1545,808) ## Meh fix to get working
+    for i in range(4):
+        hc.click()
+        sleep(3)
